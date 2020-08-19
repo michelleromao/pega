cca532a8eea009a6824af60b72617354e8f10f79
Michelle Romão <michelle.nunes10@gmail.com>
2020-08-19 17:51:43 -0300

    feat: add Footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import Illustration from './components/Illustration';
-import Delivery from './assets/illustrations/delivery.png';
-import Sun from './assets/illustrations/sunsertao.png';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-      <Illustration
-        illustrate={Delivery}
-        alt="Ilustração de uma mão entregando um pacote amarelo do Pega! a outra mão"
-        title="Entrega facilitada"
-        content="Receba sua encomenda em mãos, com o vendedor."
-      />
-      <Illustration
-        illustrate={Sun}
-        alt="Ilustração de uma mão entregando um pacote amarelo do Pega! a outra mão"
-        title="Nós vendemos no Sertão Central!"
-        content="O Pega! foi criado para vender no Sertão Central, aproximando vendedores e compradores da região."
-      />
+      <Footer />
     </>
   );
 }
