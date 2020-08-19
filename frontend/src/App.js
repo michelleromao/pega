c8719f8a4cd7a28decc6ee5dd3316f073e34cd83
Michelle Romão <michelle.nunes10@gmail.com>
2020-08-19 16:53:18 -0300

    feat: add Illustration component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import BestSeller from './components/BestSeller';
+import Illustration from './components/Illustration';
+import Delivery from './assets/illustrations/delivery.png';
+import Sun from './assets/illustrations/sunsertao.png';
 
 function App() {
   return (
     <>
-      <BestSeller nameSeller="Fernanda" />
+      <Illustration
+        illustrate={Delivery}
+        alt="Ilustração de uma mão entregando um pacote amarelo do Pega! a outra mão"
+        title="Entrega facilitada"
+        content="Receba sua encomenda em mãos, com o vendedor."
+      />
+      <Illustration
+        illustrate={Sun}
+        alt="Ilustração de uma mão entregando um pacote amarelo do Pega! a outra mão"
+        title="Nós vendemos no Sertão Central!"
+        content="O Pega! foi criado para vender no Sertão Central, aproximando vendedores e compradores da região."
+      />
     </>
   );
 }
