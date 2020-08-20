4d0091103c70db474786d2dd1b67c9b3db48a318
Yan <yanvancelis@gmail.com>
2020-08-20 19:28:16 -0300

    Agora parece que o Header vai!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ProductLarger from './components/ProductLarger';
+import Header from './components/Header/Header'
 import Coracao from './assets/icons/BatePapo'
 
 function App() {
   return (
     <>
-      <ProductLarger title="Jaquetinha Stickers" promo="20" price="45" />
-      <Coracao color="#000" />
+      <Header />
     </>
   );
 }
