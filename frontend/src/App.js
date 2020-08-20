34dadfb648555ad99379d61635651eaa7c06f804
Yan <yanvancelis@gmail.com>
2020-08-20 00:32:03 -0300

    Componentes de ícones

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ProductLarger from './components/ProductLarger';
+import Persente from './assets/Logo';
 
 function App() {
   return (
     <>
       <ProductLarger title="Jaquetinha Stickers" promo="20" price="45" />
+      <Persente color="#ccc"/>
     </>
   );
 }
