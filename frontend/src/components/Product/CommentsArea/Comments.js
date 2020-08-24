43e323118b050cba16ea8884b42055c0dad4c5de
Michelle Romão <michelle.nunes10@gmail.com>
2020-08-24 19:45:31 -0300

    Add Comments component on Announcement page.

--- frontend/src/components/Product/CommentsArea/Comments.js
+++ frontend/src/components/Product/CommentsArea/Comments.js
@@ -1,38 +1,57 @@
-import React from 'react'
-import './Comments.css'
-import Comment from './CommentItem'
-import Fernanda from '../../../assets/photos/userphoto.png'
+import React from 'react';
+import './Comments.css';
+import Comment from './CommentItem';
+import Fernanda from '../../../assets/photos/userphoto.png';
+import Vendedor from '../../../assets/photos/fotoVendedor.png';
 
-export default function Comments (props) {
-    return (
-        <div className="comments">
-            <h1>Perguntas e respostas</h1>
-            <div className="area-texto">
-                <textarea placeholder="Faça uma pergunta"></textarea>
-                <div className="botao">
-                    <div></div><input type="button" value="Enviar" />                 
-                </div>                
-            </div>
-           
-            <div className="tags">
-                <button>Prazo</button>
-                <button>Estado do produto</button>
-                <button>Entrega</button>
-                <button>Experimentar</button>
-            </div>
-
-            <div className="comentarios">
-                <Comment nome="Ferdinanda" cidade="Fortaleza-CE" foto={Fernanda}
-                pergunta="Tem algum problema?" resposta="Parece que foi fabricado ontem, novinha"
-                tag="#estado"/>
-                <Comment nome="Fernanda" cidade="Fortaleza-CE" foto={Fernanda}
-                pergunta="Tem algum problema?" resposta="Parece que foi fabricado ontem, novinha"
-                tag="#estado"/>
-                <Comment nome="Fernanda" cidade="Fortaleza-CE" foto={Fernanda}
-                pergunta="Tem algum problema?" resposta="Parece que foi fabricado ontem, novinha"
-                tag="#estado"/>
-            </div>
+export default function Comments(props) {
+  return (
+    <div className="comments">
+      <h1>Perguntas e respostas</h1>
+      <div className="area-texto">
+        <textarea placeholder="Faça uma pergunta"></textarea>
+        <div className="botao">
+          <div></div>
+          <input type="button" value="Enviar" />
         </div>
-        
-    )
+      </div>
+
+      <div className="tags">
+        <button>Prazo</button>
+        <button>Estado do produto</button>
+        <button>Entrega</button>
+        <button>Experimentar</button>
+      </div>
+
+      <div className="comentarios">
+        <Comment
+          nome="Ferdinanda"
+          cidade="Fortaleza-CE"
+          fotoComprador={Fernanda}
+          fotoVendedor={Vendedor}
+          pergunta="Tem algum problema?"
+          resposta="Parece que foi fabricado ontem, novinha"
+          tag="#estado"
+        />
+        <Comment
+          nome="Fernanda"
+          cidade="Fortaleza-CE"
+          fotoComprador={Fernanda}
+          fotoVendedor={Vendedor}
+          pergunta="Tem algum problema?"
+          resposta="Parece que foi fabricado ontem, novinha."
+          tag="#estado"
+        />
+        <Comment
+          nome="Fernanda"
+          cidade="Fortaleza-CE"
+          fotoComprador={Fernanda}
+          fotoVendedor={Vendedor}
+          pergunta="Tem algum problema?"
+          resposta="Parece que foi fabricado ontem, novinha"
+          tag="#estado"
+        />
+      </div>
+    </div>
+  );
 }
--- frontend/src/pages/Announcement/index.js
+++ frontend/src/pages/Announcement/index.js
@@ -4,13 +4,15 @@ import Breadcrumbs from '../../components/Breadcrumbs';
 import PhotosProduct from '../../components/Product/PhotosProduct';
 import ProductIformation from '../../components/Product/ProductIformation';
 import DescriptionProduct from '../../components/Product/DescriptionProduct';
+import Comment from '../../components/Product/CommentsArea/Comments';
+
 import SellerInformation from '../../components/SellerInformation';
 import ProductSmall from '../../components/ProductSmall';
 import './style.css';
 import Photoproduct from '../../assets/photos/photoproduct.png';
 import Photoproduct2 from '../../assets/photos/photoproduct2.png';
 import Photoproduct3 from '../../assets/photos/photoproduct3.png';
-import PhotoUser from '../../assets/photos/bestSeller.png';
+import PhotoUser from '../../assets/photos/bestSeller3.png';
 import PhotoProductSmall1 from '../../assets/photos/productsmall1.png';
 import PhotoProductSmall2 from '../../assets/photos/productsmall2.png';
 import PhotoProductSmall3 from '../../assets/photos/productsmall3.png';
@@ -39,14 +41,14 @@ function Announcement() {
         <DescriptionProduct description="Jaqueta rosa jeans, que vem com stickers para colocar no tecido. Rosinha claro, está um pouco desbotada em alguns cantos, na manga, mas nada demais. Possui alguns rasgos laterais, mas é do modelo. Apenas 2 meses de uso. Motivo da venda: não cabe mais em mim, nos braços ficam apertados. Veste P facilmente." />
         <SellerInformation
           photo={PhotoUser}
-          nameSeller="Fernanda"
-          location="Quixadá - CE"
+          nameSeller="Kainan"
+          location="Quixeramobim - CE"
           contFav="4"
           contComents="32"
         />
       </div>
       <div className="thirdContentProduct">
-        {/*aqui vai o component de comentário*/}
+        <Comment />
       </div>
       <div className="moreProductSeller">
         <h4>Mais desse vendedor</h4>
