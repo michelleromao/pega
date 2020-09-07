8830327a4176d293a406b07b08fc9890cfd091d3
David Castro <51448763+DavidCastroUFC@users.noreply.github.com>
2020-09-07 19:19:59 -0300

    Create Pay Safe With  Picpay component

---
+++ frontend/src/components/PaySafeWithPicpay/PaySafeWithPicpay.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {PaySafeWithStyled} from './PaySafeWithStyled';
+export default function Cupom(){
+    return (
+        <PaySafeWithStyled>
+            <p>Pague com o Picpay</p>
+            <div className="card">
+                <div className="buySafe"><p>Compre com segurança</p></div>
+                <ul>
+                    <li>
+                        <div className="listStyle"></div><div className="textList">O Pega! segura o valor pago e só libera quando a compra é finalizada com a entrega do seu pedido.</div>
+                    </li>
+                    <li>
+                        <div className="listStyle"></div><div className="textList">Clique em Finalizar compra com Picpay para efetuar o pagamento.</div>
+                    </li>
+                    
+                </ul>
+
+            </div>
+        </PaySafeWithStyled>
+    );
+}
---
+++ frontend/src/components/PaySafeWithPicpay/PaySafeWithStyled.js
@@ -0,0 +1,77 @@
+import styled from 'styled-components';
+
+export const PaySafeWithStyled = styled.div`
+    
+    p{
+        margin:0;
+        font-family: Poppins;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 24px;
+        line-height: 36px;
+        color: #569CCD;
+    }
+    .card{
+        width: 418px;
+        height: 241px;
+        background: #569CCD;
+        border: 2px solid #569CCD;
+        box-sizing: border-box;
+        border-radius: 12px;
+        margin-top: 15px;
+    }
+    .buySafe{
+        margin-left: 20px;
+        margin-top: 20px;
+        width: 247px;
+        height: 37px;
+        background: #FFFFFF;
+        border-radius: 12px;
+    }
+    .card div p{
+        font-family: Poppins;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 18px;
+        color: #569CCD;
+        text-align: center;
+    }
+    .card ul{
+       list-style-type: none;
+        padding-right: 25px;
+        font-family: Poppins;
+        text-justify: auto;
+        margin-top: 30px;
+        padding-left: 20px;
+    }
+    .card ul li{
+        display: flex;
+        flex-direction: row;
+        margin-bottom: 20px;
+    }
+    .listStyle{
+        margin-top: 5px;
+        background-color: white;
+        color: white;
+        border-radius: 100%;
+        min-width: 10px;
+        min-height: 10px;
+        max-width: 10px;
+        max-height: 10px;
+        text-align: center;
+        margin-right: 20px;
+        box-sizing: border-box;
+    }
+    .textList{
+        font-family: Poppins;
+        font-style: normal;
+        font-weight: 500;
+        font-size: 14px;
+        line-height: 21px;
+        color: #FFFFFF;
+    }
+    
+
+
+
+`;
