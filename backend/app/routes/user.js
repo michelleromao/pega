cfb961ce5d50a7770ba6a92e3c1298c66325f072
Michelle Romão <michelle.nunes10@gmail.com>
2020-09-12 21:34:12 -0300

    Create Model, Controllers, Routes of User

--- backend/app/routes/user.js
+++ backend/app/routes/user.js
@@ -1,23 +1,77 @@
-const { Router } = require("express");
+const { Router } = require('express');
 
-const users = require("../controllers/user");
+const UserController = require('../controllers/user');
 
 const usersRouter = Router();
 
-usersRouter.get("/", (request, response) => {
-  response.json(users);
-});
-
-usersRouter.get("/:id", (request, response) => {
-  const id = request.params.id;
-  const user = users.find((user) => user.id === id);
-  if (user) {
-    res.json(user);
-  } else {
-    response
-      .status(400)
-      .send("Ah! Algo de ruim aconteceu, esse user não existe aqui.");
+usersRouter.get('/', (request, response) => {
+  try {
+    const users = UserController.all();
+    return response.json({ users });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
   }
 });
 
+usersRouter.get('/:id', (request, response) => {
+  try {
+    const { id } = request.params;
+    const user = UserController.getUser(id);
+    if (user !== 'Não há usuário') {
+      return response.json({ user });
+    }
+    return response.json({ message: 'Nao foi possivel pegar' });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+usersRouter.post('/', (request, response) => {
+  try {
+    const { name, email, senha } = request.body;
+    const createUser = UserController.create({ name, email, senha });
+    return response.json(createUser);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+usersRouter.put('/:id', (request, response) => {
+  try {
+    const { id } = request.params;
+    const {
+      cpf,
+      name,
+      profilePhoto,
+      email,
+      senha,
+      telefone,
+      picpay,
+    } = request.body;
+    const editUser = UserController.editUser({
+      id,
+      cpf,
+      name,
+      profilePhoto,
+      email,
+      senha,
+      telefone,
+      picpay,
+    });
+    return response.json(editUser);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+usersRouter.delete('/:id', (request, response) => {
+  try {
+    const { id } = request.params;
+    const { reason } = request.body;
+    const deleteUser = UserController.removeUser({ id, reason });
+    return response.json(deleteUser);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
 module.exports = usersRouter;
--- backend/config/database.js
+++ backend/config/database.js
@@ -1,15 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 module.exports = function (uri) {
   mongoose.connect(uri);
-  mongoose.connection.on("connected", function () {
+  mongoose.connection.on('connected', function () {
     console.log(`Mongoose! Connected on ${uri}`);
   });
-  mongoose.connection.on("disconnected", function () {
+  mongoose.connection.on('disconnected', function () {
     console.log(`Mongoose! Desconnected of ${uri}`);
   });
-  mongoose.connection.on("error", function () {
+  mongoose.connection.on('error', function () {
     console.log(`Mongoose! Error on connection ${uri}`);
   });
-  mongoose.set("debug", true);
+  mongoose.set('debug', true);
 };
