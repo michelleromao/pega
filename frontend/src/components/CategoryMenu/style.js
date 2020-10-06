0b2b17099f63f054d36efe9a007bcc201594ed7c
Yan <yanvancelis@gmail.com>
2020-10-06 16:03:03 -0300

    Correção das categorias do Header (agora ocupam todo o espaço de forma correta)

---
+++ frontend/src/components/CategoryMenu/style.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Categorias = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin:10px 0 5px 0;
+    transition: 0.2s;
+    a {
+        font-family: 'Poppins';
+        font-size: 14px;
+        font-weight: 600;
+        color: #202020;
+    }
+
+    a:hover {
+        color: #383838;
+        transition: 0.2s;
+    }
+`
