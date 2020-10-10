ed0b3b680850addbcf3e712bd6d4ad703a8cac44
Yan <yanvancelis@gmail.com>
2020-10-09 21:54:43 -0300

    Menu do painel funcionando

--- frontend/src/pages/UserPanel/Index.js
+++ frontend/src/pages/UserPanel/Index.js
@@ -1,18 +1,86 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import PanelMenu from '../../components/PanelMenu'
 import PanelContent from '../../components/PanelContent'
-import {PanelUser, Title} from './style'
+import {PanelUser, Title, Content, Sidebar, Hove} from './style'
 import ProductPanel from '../../components/ProductPanel'
+import Foto from '../../assets/photos/userphoto.png'
+import { Link } from 'react-router-dom';
 
 export default function Panel() {
+    
+    const [infos, setInfos] = useState(false);
+    const [compras, setCompras] = useState(false);
+    const [anuncios, setAnuncios] = useState(false);
+    const [enderecos, setEnderecos] = useState(false);
+    const [favoritos, setFavoritos] = useState(false);
+    const [pagamentos, setPagamentos] = useState(false);
+
+    function resetState() {
+        setInfos(false)
+        setCompras(false)
+        setAnuncios(false)
+        setEnderecos(false)
+        setFavoritos(false)
+        setPagamentos(false)
+    }
+    
+    function click (hook) {
+        resetState()
+        hook(!false)
+    }
+
     return (
         <>        
         <PanelUser>
-            <PanelMenu></PanelMenu>
-            <PanelContent>
-                <ProductPanel nome="Camisa Vasco Kappa 2020" notify="1" valor="50" vendedor="Michelle" data="15 Novembro"></ProductPanel>
-            </PanelContent>
-            
+        <Sidebar>
+            <p>Fernanda</p>
+            <img src={Foto}></img>
+            <ul>                
+                <button onClick={() => click(setInfos)}>Informações pessoais<Hove></Hove></button>
+                <button onClick={() => click(setCompras)}>Minhas compras<Hove></Hove></button>
+                <button onClick={() => click(setAnuncios)}>Meus anúncios<Hove></Hove></button>
+                <button onClick={() => click(setEnderecos)}>Meus endereços<Hove></Hove></button>
+                <button onClick={() => click(setFavoritos)}>Meus favoritos<Hove></Hove></button>
+                <button onClick={() => click(setPagamentos)}>Pagamentos<Hove></Hove></button>
+            </ul>
+        </Sidebar>
+            <Content>
+                {infos ? 
+                    <ProductPanel nome="Camisa Vasco Kappa 2020" valorAdicionado="20" rastreamento="AMASDASDQWEBR" formaPagamento="PicPay" formaEntrega="Sedex" tipoPagamento="PicPay" tamanho="M" estado="Nova" cor="Preto" notify="1" valor="50" vendedor="Michelle" data="15 Novembro"></ProductPanel>
+                        
+                 : <div></div> 
+                }
+
+                {compras ? 
+                    <h4>Compras</h4>
+                        
+                 : <div></div> 
+                }
+
+                {anuncios ? 
+                    <h4>Anúncios</h4>
+                        
+                 : <div></div> 
+                }
+
+                {enderecos ? 
+                    <h4>Endereços</h4>
+                        
+                 : <div></div> 
+                }
+
+                {favoritos ? 
+                    <h4>Favoritos</h4>
+                        
+                 : <div></div> 
+                }
+
+                {pagamentos ? 
+                    <h4>Pagamentos</h4>
+                        
+                 : <div></div> 
+                }
+            </Content>          
         </PanelUser>            
         </>
     )
--- frontend/src/pages/UserPanel/style.js
+++ frontend/src/pages/UserPanel/style.js
@@ -25,3 +25,87 @@ export const Title = styled.h4`
     font-size:26px;
     font-family:'Poppins';
 `
+
+export const Content = styled.div`
+        border-radius: 0 12px 12px 0;
+        border: solid 1px #83BDE6;
+        border-left: 0px;
+        width: 800px;
+        height: 730px;
+        padding: 50px;
+    }
+`
+
+export const Favoritos = styled.div`
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+`
+
+export const Sidebar = styled.div`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 260px;
+        background-color: #83BDE6;   
+        border-radius: 12px; 
+        height: 100%;
+        padding: 50px 0 50px 10px;
+    }
+
+    img {
+        width: 120px;
+        border: solid 5px #fff;
+        border-radius: 50%;
+    }
+
+    ul {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        height:440px;
+        margin-top:40px;
+        width: 250px;
+        display: flex;
+        flex-direction: column;
+        text-align: right;
+    }
+
+    button {
+        background: transparent;
+        border: none;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        font-family: 'Poppins';
+        color: #fff;    
+        font-size: 15px;
+        height: 20px;
+        padding: 25px 0 25px 35px;   
+    }   
+    button:hover{
+        background:#fff;
+        color: #83BDE6;
+        cursor: pointer;
+        border-radius:10px 0 0 10px;
+        transition: 0.2s;
+    } 
+
+    button:focus{
+        background:#fff;
+        color: #83BDE6;
+        cursor: pointer;
+        border-radius:10px 0 0 10px;
+    }
+`
+export const Hove = styled.div`
+        padding: 15px 0 15px 15px;  
+        height: 20px;
+        background-color:#fff;
+        border-radius: 7px 0 0 7px;
+    }
+`
+
